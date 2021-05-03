3449a2d305aa05aa6c713d5e4148b4e47d33a9ea
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-03 18:27:29 -0300

    Removed deprecated function

--- mobile/NiceEvaluation/App.js
+++ mobile/NiceEvaluation/App.js
@@ -2,34 +2,16 @@ import * as ScreenOrientation from 'expo-screen-orientation';
 import React, { useState } from 'react';
 import { StyleSheet, View , StatusBar, Alert } from 'react-native';
 import { Login, Home } from './pages';
+import sendForm from './communication/sendform';
 const network = require('./configs/network.json');
 
 async function lockOrientation(){
   await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
 }
 
-function sendLogin(login){
-  return new Promise((resolve, reject)=>{
-    let request = '';
-    for(const key in login){
-      request+= key+'='+login[key]+'&'
-    }
-    fetch(network.uri, {
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: request
-    }).then(resp=>resp.json()).then(resp=>{
-      resolve(resp)
-    }).catch(()=>{
-      reject('error fetch')
-    })
-  })
-}
-
 async function tryLogin(login){
-  const resp = await sendLogin(login)
+  const resp = await sendForm(network.uri, login)
+  console.log(resp)
   if(resp.access == 'granted'){
     Alert.alert('Acesso liberado')
     return true
