909c2af1527cf06785186b0765f7d0217456a914
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-03 08:55:33 -0300

    Added network communication

--- mobile/NiceEvaluation/App.js
+++ mobile/NiceEvaluation/App.js
@@ -1,17 +1,52 @@
 import * as ScreenOrientation from 'expo-screen-orientation';
 import React from 'react';
-import { StyleSheet, Text, View , StatusBar } from 'react-native';
+import { StyleSheet, View , StatusBar, Alert } from 'react-native';
 import Login from './pages/login';
+const network = require('./configs/network.json');
 
 async function lockOrientation(){
   await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+}
+
+function sendLogin(login){
+  return new Promise((resolve, reject)=>{
+    let request = '';
+    for(const key in login){
+      request+= key+'='+login[key]+'&'
+    }
+    fetch(network.uri, {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: request
+    }).then(resp=>resp.json()).then(resp=>{
+      resolve(resp)
+    }).catch(()=>{
+      reject('error fetch')
+    })
+  })
+}
+
+async function tryLogin(login){
+  const resp = await sendLogin(login)
+  if(resp.access == 'granted')
+    Alert.alert('Acesso liberado')
+  else
+    Alert.alert('Acesso negado')
 }
 
 export default function App() {
   lockOrientation()
   return (
     <View style={styles.container}>
-      <Login/>
+      <Login
+        onSubmit={
+          submit => {
+            tryLogin(submit)
+          }
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
