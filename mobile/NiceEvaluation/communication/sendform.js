32d877c77e2aa73511adea4706621f5a8937b3d7
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-04 14:03:50 -0300

    Added comments in communication/sendform

--- mobile/NiceEvaluation/communication/sendform.js
+++ mobile/NiceEvaluation/communication/sendform.js
@@ -1,13 +1,20 @@
 export default function sendForm(url, form){
   return new Promise((resolve, reject)=>{
+    //Variable initialization
     let request = ''
+
+    //Mounting the request in url format
     for(const key in form){
       request += key+'='+form[key]+'&'
     }
+    
+    //Configuring the request
     const req = new XMLHttpRequest()
     req.open('POST', url, true)
     req.responseType = 'json'
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
+    //When loaded the request...
     req.addEventListener('load', ()=>{
       if(req.status !== 200){
 	reject('Erro de comunicação: '+req.statusText)
@@ -20,12 +27,13 @@ export default function sendForm(url, form){
 	reject('Resposta vazia')
     }, false)
 
+    //When request throw error...
     req.addEventListener('error', ()=>{
       console.log('Erro: ', arguments)
       console.log(req)
       reject('Erro ajax')
     }, false)
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
     req.send(request)
   })
 }
