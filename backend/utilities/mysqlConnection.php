05cb0c791e865bff56d3fb231c3265f522613649
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-05 17:28:51 -0300

    Added mysql connections functions

---
+++ backend/utilities/mysqlConnection.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+function mysqlConnection(string $database, string $user, string $passwd):mysqli{
+	$connection = new mysqli($database, $user, $passwd, 'NiceEvaluation');
+	if($connection->connect_error)
+		throw new Exception('Conexão recusada');
+	return $connection;
+}
+
+function mysqlQuery(mysqli $connection, string $query){
+	$request = $connection->query($query);
+	if(!$request)
+		throw new Exception('Erro de comunicação');
+	return $request;
+}
+?>
