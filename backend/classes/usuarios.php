1555b404d37aefdafcaa142943a94125c7156ffb
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-06 10:33:25 -0300

    Populed Administrador::criarUsuario function

--- backend/classes/usuarios.php
+++ backend/classes/usuarios.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 require_once(__DIR__.'/pedido.php');
 require_once(__DIR__.'/tema.php');
+require_once(__DIR__.'/../utilities/hashAndSalt.php');
+require_once(__DIR__.'/../utilities/mysqlConnection.php');
 
 class Usuario{
 	protected $nome;
@@ -19,7 +21,7 @@ class Usuario{
 	}
 
 	public static function login(string $senha, string $id){
-	
+		
 	}
 
 	public function logout(){
@@ -62,16 +64,53 @@ class Usuario{
 class Professor extends Usuario{}
 
 class Administrador extends Usuario{
-	public function criarUsuario(Usuario $dados, bool $adm):bool{
-	
+	public function criarUsuario(Usuario $dados, bool $adm=false):bool{
+		$salt = genSalt();
+		$passwdAndSalt = $dados->senha.$salt;
+		$hashAndSalt = genHash($passwdAndSalt);
+
+		try{
+			$connection = mysqlConnection('localhost', 'root', 'senha');
+			mysqlQuery($connection, 'INSERT Usuario(nome, nomeUsuario, email, hashAndSalt, salt)
+				VALUES(
+				"'.$dados->nome.'",
+				"'.$dados->nomeUsuario.'",
+				"'.$dados->email.'",
+				"'.$hashAndSalt.'",
+				"'.$salt.'");');
+		}catch(Exception $e){
+			return false;
+		}
+
+		try{
+			if($adm){
+				mysqlQuery($connection, 'INSERT Administrador(Usuario_nomeUsuario, Usuario_email)
+					VALUES("'.$dados->nomeUsuario.'", "'.$dados->email.'");');
+			}else{
+				mysqlQuery($connection, 'INSERT Professor(
+					Usuario_nomeUsuario,
+					Usuario_email
+					Administrador_Usuario_nomeUsuario,
+					Administrador_Usuario_email)
+					VALUES(
+					"'.$dados->nomeUsuario.'",
+					"'.$dados->email.'",
+					"'.$this->nomeUsuario.'",
+					"'.$this->email.'");');
+			}
+		}catch(Exception $e){
+			mysqlQuery($connection, 'DELETE FROM Usuario WHERE email="'.$dados->email.'";');
+			return false;
+		}
+		return true;
 	}
 
 	public function editarUsuario(Usuario $usuario, string $dado):bool{
-	
+
 	}
 
 	public function removerUsuario(string $email):bool{
-	
+		
 	}
 
 	public function listarUsuarios():array{
@@ -79,7 +118,7 @@ class Administrador extends Usuario{
 	}
 
 	public function criarQuestao(string $corpo, string $resposta, bool $objetiva, array|null $itens=null):bool{
-	
+		
 	}
 
 	public function excluirQuestoes(array $ids):bool{
