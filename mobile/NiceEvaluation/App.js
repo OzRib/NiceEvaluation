82cf26a9a9a30405edb625c33d8c0480553a697d
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-11 19:42:43 -0300

    Refactored code to react-navigation

--- mobile/NiceEvaluation/App.js
+++ mobile/NiceEvaluation/App.js
@@ -1,9 +1,13 @@
 //Imports and initiation variables
+import 'react-native-gesture-handler';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import React, { useState } from 'react';
 import { StyleSheet, View , StatusBar, Alert } from 'react-native';
 import { Login, Home } from './pages';
 import sendForm from './communication/sendform';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator()
 const network = require('./configs/network.json');
 
 async function lockOrientation(){
@@ -16,44 +20,47 @@ async function tryLogin(login){
   return resp
 }
 
-//Page rendered in app
-function Page({logged, submit}){
-  if(logged)
-    return(<Home/>)
-  else{
-    return(
-      <Login 
-	onSubmit={
-	  async data=>{
-	    const resp = await tryLogin(data)
-	    submit(resp.access == 'granted')
-	    if(resp.error){
-	      return({
-		error: resp.error,
-		showError:true
-	      })
-	    }else{
-	      return null
-	    }
-	  }
-	}
-      />
-    )
-  }
-}
-
 //App
 export default function App(){
-  const [logged, setLogged] = useState(false)
   lockOrientation()
   return (
-    <View>
-      <Page 
-	logged={logged}
-	submit={setLogged}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" mode="modal">
+	<Stack.Screen 
+	  name="Login"
+	  options={{
+	    headerShown: false
+	  }}
+	>
+	  {
+	    ({navigation})=>(
+	      <Login
+		onSubmit={
+		  async data =>{
+		    const resp = await tryLogin(data)
+		    if(resp.access == 'granted')
+		      navigation.navigate('Home')
+		    if(resp.error){
+		     return({
+			error: resp.error,
+			showError: true
+		      })
+		    }else{
+		      return({
+			error: null,
+			showError: false
+		      })
+		    }
+		  }
+		}
+	      />
+	    )
+	  }
+	</Stack.Screen>
+	<Stack.Screen name="Home" component={Home}/>
+      </Stack.Navigator>
+      <StatusBar barStyle="auto"/>
+    </NavigationContainer>
   );
 }
 
