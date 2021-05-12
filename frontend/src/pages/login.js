b6ef0b057cd7fdff0b0be7b916e75c823b631e61
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-12 16:37:33 -0300

    Added login page

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,23 @@
+import './login.css'
+import { Form, Button } from 'react-bootstrap';
+
+export default function Login(){
+    return(
+        <div className="login centered">
+            <Form name="login" className="flex">
+                <Form.Label>
+                    <h3 className="whiteText boldText">Nome de Usuário/Email</h3>
+                </Form.Label>
+                <Form.Control type="text" name="id" id="id"/>
+                <Form.Label>
+                    <h3 className="whiteText boldText">Senha</h3>
+                </Form.Label>
+                <Form.Control type="password" name="senha" id="senha"/>
+                <Button variant="primary">
+                    Entrar
+                </Button>
+                <a href="/#">Esqueci minha senha</a>
+            </Form>
+        </div>
+    )
+}
