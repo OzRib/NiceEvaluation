284ddb5db87e79f73a9ea1c8406d5eda25c72dcb
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-13 12:47:13 -0300

    Debugging the router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, HashRouter } from 'react-router-dom';
-import * as Pages from './pages';
+import { Switch, Route, HashRouter } from 'react-router-dom';
+import { Login } from './pages';
 
 function App() {
-  let Routes = []
-  for(const x in Pages){
-    Routes.push(Pages[x])
-  }
-
   return (
     <div className="flexColumn">
       <HashRouter>
-        {Routes.map((Obj, key)=>(
-          <Route key={key} exact path={
-            Obj.name === 'Login' ? '/': '/'+Obj.name
-          } component={Obj}/>
-        ))}
+        <Switch>
+          <Route exact path='/' component={Login}/>
+        </Switch>
       </HashRouter>
     </div>
   );
