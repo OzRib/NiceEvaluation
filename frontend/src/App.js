855686fffa6eb065ba91bca19a21c8d5ff3dcca1
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-16 14:45:26 -0300

    Added new routes in HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route, HashRouter } from 'react-router-dom';
-import { Login } from './pages';
+import { Login, AdminChoice, ManageUsers } from './pages';
 
 function App() {
   return (
@@ -10,6 +10,8 @@ function App() {
       <HashRouter>
         <Switch>
           <Route exact path='/' component={Login}/>
+          <Route exact path='/admin-choice' component={AdminChoice}/>
+          <Route exact path='/manage-users' component={ManageUsers}/>
         </Switch>
       </HashRouter>
     </div>
