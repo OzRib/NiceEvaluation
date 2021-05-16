4633145a4a8cbcbd97ab641be8cab67857a6e20c
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-16 16:55:23 -0300

    Added confirmation functions

---
+++ backend/utilities/confirmations.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+require_once(__DIR__.'/mysqlConnection.php');
+require_once(__DIR__.'/../classes/usuarios.php');
+
+function isUserAdmin(array $userData):bool{
+    $userData = getAdminData($userData['nomeUsuario']);
+    $isAdmin = $userData ? true : false;
+    return $isAdmin;
+}
+?>
