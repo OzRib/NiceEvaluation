0027887210cc171c5671e3f294c96082ad746942
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-17 19:31:09 -0300

    Initiate the env settings

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fd029ed08e662ea325ae863810aaadca3ee681d3',
+    'name' => 'ozeias/env',
+  ),
+  'versions' => 
+  array (
+    'ozeias/env' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd029ed08e662ea325ae863810aaadca3ee681d3',
+    ),
+  ),
+);
