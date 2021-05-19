ca63e6eb07c6b17d6be6c8895b13f03b358f16fe
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-18 23:11:40 -0300

    Redirect control in admin-choice.js

--- frontend/src/pages/admin-choice/admin-choice.js
+++ frontend/src/pages/admin-choice/admin-choice.js
@@ -3,6 +3,19 @@ import './admin-choice.css'
 import logo from '../../logo.png';
 
 export default function AdminChoice(){
+
+    async function onLoad(){
+        const actions = {
+            'user': function(){
+                window.location.href = '/#/teacher-page'
+            },
+            'error': function(){
+                window.location.href = '/#/'
+            }
+        }
+    } 
+
+    onLoad()
     return(
         <div className="admin-choice flexColumn AICenter fullscreen">
             <Image className="mediumLogo" src={logo}></Image>
