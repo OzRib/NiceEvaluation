80ee3cb17be4300ecffda24634002d2aa07bfdfb
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-20 11:26:55 -0300

    Updated margin horizontal of contentbox to 10%

--- frontend/src/components/contentbox.js
+++ frontend/src/components/contentbox.js
@@ -1,6 +1,6 @@
 export default function ContentBox({children, className, ...props}){
     return(
-        <div className={"bgcWhite mh20p p1o8 border-rad20p mt1p shadow "+className} {...props}>
+        <div className={"bgcWhite mh10p p1o8 border-rad20p mt1p shadow "+className} {...props}>
             {children}
         </div>
     )
