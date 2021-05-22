677602b5909c8e9568b91843f15494414ec9bb76
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-22 12:26:39 -0300

    Updated wrapper of communication

--- frontend/src/communication/index.js
+++ frontend/src/communication/index.js
@@ -2,5 +2,16 @@ import checkLogin from './checklogin';
 import tryLogin from './trylogin';
 import userControl from './usercontrol';
 import logout from './logout';
+import rmUser from './rmuser';
+import addUser from './adduser';
+import editUser from './edituser';
 
-export { checkLogin, tryLogin, userControl, logout }
+export { 
+	checkLogin, 
+	tryLogin, 
+	userControl, 
+	logout, 
+	rmUser, 
+	addUser,
+	editUser
+}
