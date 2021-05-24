b103908a753ba53df7d141f28fa8500e520ba6dd
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-24 16:35:57 -0300

    Updated wrapper of pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,5 +2,6 @@ import Login from './login/login';
 import AdminChoice from './admin-choice/admin-choice';
 import ManageUsers from './manage-users/manage-users';
 import LoadingPage from './loading-page';
+import TeacherPage from './teacher-page/teacher-page'
 
-export { Login, AdminChoice, ManageUsers, LoadingPage }
+export { Login, AdminChoice, ManageUsers, LoadingPage, TeacherPage }
