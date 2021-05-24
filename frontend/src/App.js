f19a10447f8bd60f88fd03a27b53d444315d0d07
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-24 16:35:26 -0300

    Updated router in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route, HashRouter } from 'react-router-dom';
-import { Login, AdminChoice, ManageUsers } from './pages';
+import { Login, AdminChoice, ManageUsers, TeacherPage } from './pages';
 
 function App() {
   return (
     <div className="flexColumn">
       <HashRouter>
-        <Switch>
-          <Route exact path='/' component={Login}/>
-          <Route exact path='/admin-choice' component={AdminChoice}/>
-          <Route exact path='/manage-users' component={ManageUsers}/>
-        </Switch>
+	<Route exact path='/' component={Login}/>
+	<Route exact path='/admin-choice' component={AdminChoice}/>
+	<Route exact path='/manage-users' component={ManageUsers}/
+	<Route exact path='/teacher-page' component={TeacherPage}/>
       </HashRouter>
     </div>
   );
