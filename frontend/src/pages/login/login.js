7644e208fb875682678d46adef124d441bee2162
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-24 16:30:06 -0300

    Added routing manage in pages/login

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -3,8 +3,10 @@ import './login.css';
 import logo from '../../logo.png';
 import { Form, Button, Alert, Image } from 'react-bootstrap';
 import { tryLogin, checkLogin, userControl } from '../../communication';
+import { LoadingPage } from '../';
 
 export default function Login(){
+    const [loaded, setLoaded] = React.useState(false)
     const [show, setShow] = React.useState(false)
     const [error, setError] = React.useState(null)
 
@@ -22,6 +24,8 @@ export default function Login(){
         const action = userControl(resp)
         if(actions[action.action] !== undefined)
             actions[action.action]()
+	else
+	    setLoaded(true)
     }
 
     function showError(){
@@ -56,27 +60,34 @@ export default function Login(){
             actions[action.action](action.error)
     }
 
-    onLoad()
+    React.useEffect(async ()=>{
+	await onLoad()
+    })
+
     return(
-        <div className="login flexColumn AICenter">
-            <Image className="mediumLogo" src={logo}></Image>
-            <Form name="login" className="flexColumn AICenter" id="login" onSubmit={event => handleSubmit(event)}>
-                <Form.Label className="label" htmlFor="id">
-                    <div className="whiteText boldText f1o5">Nome de Usuário ou Email</div>
-                </Form.Label>
-                <Form.Control type="text" name="id" id="id" placeholder="Insira seu nome de usuário ou Email"/>
-                <Form.Label className="label" htmlFor="senha">
-                    <div className="whiteText boldText f1o5">Senha</div>
-                </Form.Label>
-                <Form.Control type="password" name="senha" id="senha" placeholder="Insira sua senha"/>
-                <Button variant="success" type="submit" id="submit">
-                    Entrar
-                </Button>
-                <a href="/#" class="newLink boldText">Esqueci minha senha</a>
-            </Form>
-            <Alert variant="danger" show={show}>
-                {error}
-            </Alert>
+        <div id="login">
+	    {!loaded && <LoadingPage/>}
+	    {loaded &&
+	    <div className="flexColumn AICenter fullscreen pt7p">
+		<Image className="mediumLogo" src={logo}></Image>
+		<Form name="login" className="flexColumn AICenter" id="login" onSubmit={event => handleSubmit(event)}>
+		    <Form.Label className="label" htmlFor="id">
+			<div className="whiteText boldText f1o5">Nome de Usuário ou Email</div>
+		    </Form.Label>
+		    <Form.Control type="text" name="id" id="id" placeholder="Insira seu nome de usuário ou Email"/>
+		    <Form.Label className="label" htmlFor="senha">
+			<div className="whiteText boldText f1o5">Senha</div>
+		    </Form.Label>
+		    <Form.Control type="password" name="senha" id="senha" placeholder="Insira sua senha"/>
+		    <Button variant="success" type="submit" id="submit">
+			Entrar
+		    </Button>
+		    <a href="/#" class="newLink boldText">Esqueci minha senha</a>
+		</Form>
+		<Alert variant="danger" show={show}>
+		    {error}
+		</Alert>
+	    </div>}
         </div>
     )
 }
