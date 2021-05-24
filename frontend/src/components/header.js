6b5f6254ac9387237cc9131e4bfcf58a3161496b
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-24 15:57:21 -0300

    Renamed adminheader to header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,14 +2,14 @@ import { Image, Dropdown, DropdownButton, } from 'react-bootstrap';
 import IconLogo from './iconlogo';
 import { logout } from '../communication';
 
-export default function AdminHeader(){
+export default function Header(){
     async function handleClick(){
 	const resp = await logout()
 	if(resp.logged === false)
 	    window.location.href = '/#/'
     }
     return(
-        <div id="header" className="fullWidth flexRow AICenter shadow">
+        <div className="fullWidth flexRow AICenter bgcRoyal shadow">
             <IconLogo/>
             <div 
                 className="perfil flexRow w100p JCFlexEnd"
