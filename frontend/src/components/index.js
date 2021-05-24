d03d1453e569df87893294e12ac9f409be5a18bd
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-24 16:02:47 -0300

    Updated wrapper of components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,21 +1,27 @@
 import ContentBox from './contentbox';
 import IconLogo from './iconlogo';
 import AddUser from './adduser';
-import AdminHeader from './adminheader';
+import Header from './header';
 import ListUsers from './listusers.js';
 import User from './user';
 import EditUserForm from './edituserform';
 import ShowUser from './showuser';
 import SuccessAlert from './successalert';
+import Search from './search';
+import Subject from './subject';
+import ListSubjects from './listsubjects';
 
 export { 
 	IconLogo, 
 	ContentBox, 
 	AddUser, 
-	AdminHeader, 
+	Header, 
 	ListUsers, 
 	User, 
 	EditUserForm, 
 	ShowUser,
-	SuccessAlert
+	SuccessAlert,
+	Search,
+	Subject,
+	ListSubjects
 }
