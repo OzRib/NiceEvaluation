febc4a42149b80e395832f479ec8eed2494d0c2c
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-25 19:34:35 -0300

    Refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route, HashRouter } from 'react-router-dom';
-import { Login, AdminChoice, ManageUsers, TeacherPage } from './pages';
+import { 
+	Login, 
+	AdminChoice, 
+	ManageUsers, 
+	TeacherPage, 
+	Subject,
+	Generate,
+	ShowQuestions
+} from './pages';
 
 function App() {
   return (
