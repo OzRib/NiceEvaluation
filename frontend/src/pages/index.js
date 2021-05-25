6e2fb4058d38148317746095f74688064a7941d9
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-25 19:32:18 -0300

    Updating pages wrapper

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,6 +2,17 @@ import Login from './login/login';
 import AdminChoice from './admin-choice/admin-choice';
 import ManageUsers from './manage-users/manage-users';
 import LoadingPage from './loading-page';
-import TeacherPage from './teacher-page/teacher-page'
+import TeacherPage from './teacher-page/teacher-page';
+import Subject from './subject/subject.js';
+import Generate from './generate/generate.js';
+import ShowQuestions from './show-questions/show-questions';
 
-export { Login, AdminChoice, ManageUsers, LoadingPage, TeacherPage }
+export { Login, 
+	AdminChoice, 
+	ManageUsers, 
+	LoadingPage, 
+	TeacherPage, 
+	Subject,
+	Generate,
+	ShowQuestions
+}
