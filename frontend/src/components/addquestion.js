7a1fa007a6ce55bbd7d5d5253bcebbad7db8258f
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-27 20:21:18 -0300

    Added components AddQuestion

---
+++ frontend/src/components/addquestion.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import AddQuestionForm from './addquestionform';
+import { Modal, Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+export default class AddQuestion extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			show: false
+		}
+	}
+
+	addItem(){
+		const items = this.state.items
+	}
+
+	handleClick(){
+		this.setState({show:true})
+	}
+
+	handleClose(){
+		this.setState({show:false})
+	}
+
+	render(){
+		const show = this.state.show
+		const id = this.props.subjectId
+
+		return(
+		<>
+			<Button
+				className="w100p"
+				variant="outline-success"
+				onClick={()=>{this.handleClick()}}
+			>
+				<FontAwesomeIcon icon={faPlus}/>{' '}
+				Adicionar Questão
+			</Button>
+			<Modal 
+				show={show} 
+				onHide={()=>{this.handleClose()}}
+			>
+				<Modal.Header closeButton>
+					<Modal.Title>
+						Adicionar Questão
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<AddQuestionForm
+						subjectId={id}
+					/>	
+				</Modal.Body>
+			</Modal>
+		</>
+		)
+	}
+}
