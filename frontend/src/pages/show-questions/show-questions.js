f331e0f748163a5fed34a23f479fa12a0f20abb9
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-05-27 20:58:52 -0300

    Updating pages/show-questions

--- frontend/src/pages/show-questions/show-questions.js
+++ frontend/src/pages/show-questions/show-questions.js
@@ -1,11 +1,17 @@
-import { Header, ContentBox } from '../../components';
+import { Header, ContentBox, AddQuestion, ListThemes } from '../../components';
 
 export default function ShowQuestions({match:{params}}){
 	return(
 		<div className="flexColumn fullscreen bgcPrimary">
 			<Header/>
 			<ContentBox>
+				<AddQuestion
+					subjectId={params.id}
+				/>
 			</ContentBox>
+			<ListThemes
+				subjectId={params.id}
+			/>
 		</div>
 	)
 }
