4a804c1dd0613019543d2c570d9711daa03c7077
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-01 10:32:31 -0300

    Added user control in pages/subject

--- frontend/src/pages/subject/subject.js
+++ frontend/src/pages/subject/subject.js
@@ -1,14 +1,45 @@
-import './subject.css'
-import { Header, Choice } from '../../components'
+import React from 'react';
+import './subject.css';
+import { Header, Choice } from '../../components';
+import { checkLogin, userControl } from '../../communication';
+import { LoadingPage} from '../';
 
 export default function Subject({match:{params}}){
-	console.log(params)
+	const [loaded, setLoaded] = React.useState(false)
+
+	async function onLoad(){
+		const actions = {
+			'error': function(){
+				window.location.href = '/#/'
+			}
+		}
+
+		const resp = await checkLogin()
+		const action = userControl(resp)
+
+		if(actions[action.action] !== undefined){
+			actions[action.action]()
+			return false
+		}else{
+			return true
+		}
+
+	}
+
+	React.useEffect(async ()=>{
+		setLoaded(await onLoad())
+	},[])
+
 	return(
 		<div  id="subject" className="flexColumn fullscreen">
-			<Header/>
-			<Choice 
-				subjectId={params.id}	
-			/>
+			{!loaded && <LoadingPage/>}
+			{loaded &&
+			<>
+				<Header/>
+				<Choice 
+					subjectId={params.id}	
+				/>
+			</>}
 		</div>
 	)
 }
