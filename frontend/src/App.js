c70dcf5fd9612e58c2d72f67841f83a81b17b781
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-01 10:38:40 -0300

    Removed extra div of App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,18 +15,16 @@ import {
 
 function App() {
   return (
-    <div className="flexColumn">
-      <HashRouter>
-	<Route exact path='/' component={Login}/>
-	<Route exact path='/admin-choice' component={AdminChoice}/>
-	<Route exact path='/manage-users' component={ManageUsers}/>
-	<Route exact path='/teacher-page' component={TeacherPage}/>
-	<Route exact path='/subject/:id' component={Subject}/>
-	<Route exact path='/subject/:id/generate' component={Generate}/>
-	<Route exact path='/subject/:id/show-questions' component={ShowQuestions}/>
-	<Route exact path='/subject/:subjId/theme/:theme' component={Theme}/>
-      </HashRouter>
-    </div>
+    <HashRouter>
+      <Route exact path='/' component={Login}/>
+      <Route exact path='/admin-choice' component={AdminChoice}/>
+      <Route exact path='/manage-users' component={ManageUsers}/>
+      <Route exact path='/teacher-page' component={TeacherPage}/>
+      <Route exact path='/subject/:id' component={Subject}/>
+      <Route exact path='/subject/:id/generate' component={Generate}/>
+      <Route exact path='/subject/:id/show-questions' component={ShowQuestions}/>
+      <Route exact path='/subject/:subjId/theme/:theme' component={Theme}/>
+    </HashRouter>
   );
 }
 
