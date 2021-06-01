2ba924331853c7170897a02d245c7f20af117b15
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-01 10:58:42 -0300

    Added workflow/getNoThemeQuestions.php

---
+++ backend/workflow/getNoThemeQuestions.php
@@ -0,0 +1,44 @@
+<?php
+require_once(__DIR__.'/../utilities/mysqlConnection.php');
+
+try{
+	session_start();
+	if(empty($_SESSION['usuario']))
+		throw new Exception('Usuário não logado');
+
+	if($_POST['materia'] === null)
+		throw new Exception('É necessário uma matéria');
+
+	$subjectId = (int) $_POST['materia'];
+	$subjects = listSubjects();
+	$subject = $subjects[$subjectId];
+	$subjectName = $subject['nome'];
+
+	$questions = listQuestionsWithoutTheme($subjectName);
+
+	$questionSkeleton = [
+		0=>'id',
+		1=>'itens',
+		2=>'corpo',
+		3=>'resposta'
+	];
+	$tempQuestion = [];
+	$finalQuestions = [];
+
+	foreach($questions as $number=>$question){
+		foreach($question as $key=>$value){
+			$tempQuestion[$questionSkeleton[$key]] = $value;
+		}
+		if(!($tempQuestion['itens'] === null))
+			$tempQuestion['itens'] = json_decode($tempQuestion['itens']);
+
+		$finalQuestions[] = $tempQuestion;
+	}
+
+	$resp = $finalQuestions;
+}catch(Exception $e){
+	$resp = ['error'=>$e->getMessage()];
+}
+
+echo(json_encode($resp));
+?>
