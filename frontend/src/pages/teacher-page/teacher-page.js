bf55b4ac9085a247410c329865678d9d5c02e86d
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-01 10:28:20 -0300

    Added handle routing in pages/teacher-page

--- frontend/src/pages/teacher-page/teacher-page.js
+++ frontend/src/pages/teacher-page/teacher-page.js
@@ -1,9 +1,36 @@
+import React from 'react';
 import './teacher-page.css';
 import { Header, ContentBox, Search, ListSubjects } from '../../components'
+import { LoadingPage } from '../';
+import { checkLogin, userControl } from '../../communication';
 
 export default function TeacherPage(){
+	const [loaded, setLoaded] = React.useState(false)
+
+	async function onLoad(){
+		const actions = {
+			'error': function(){
+				window.location.href = '/#/'
+			}
+		}
+
+		const resp = await checkLogin()
+		const action = await userControl(resp)
+		if(actions[action.action] !== undefined)
+			actions[action.action]()
+		else
+			setLoaded(true)
+	}
+
+	React.useEffect(async ()=>{
+		await onLoad()
+	},[])
+
 	return(
 		<div id="teacher-page" className="flexColumn fullscreen">
+		{!loaded && <LoadingPage/>}
+		{loaded &&
+		<>
 			<Header/>
 			<ContentBox className="flexColumn">
 				<Search
@@ -14,6 +41,7 @@ export default function TeacherPage(){
 				<h6>Matérias: </h6>
 				<ListSubjects/>
 			</ContentBox>
+		</>}
 		</div>
 	)	
 }
