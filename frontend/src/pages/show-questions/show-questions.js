87466111ad48de7d173d01ab70ad033e183cd34d
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-01 10:30:20 -0300

    Added user control in pages/show-questions

--- frontend/src/pages/show-questions/show-questions.js
+++ frontend/src/pages/show-questions/show-questions.js
@@ -1,17 +1,58 @@
+import React from 'react';
 import { Header, ContentBox, AddQuestion, ListThemes } from '../../components';
+import { LoadingPage } from '../';
+import { checkLogin, userControl } from '../../communication';
 
 export default function ShowQuestions({match:{params}}){
+	const [loaded, setLoaded] = React.useState(false)
+	const [showAdd, setShowAdd] = React.useState(false)
+
+	async function onLoad(){
+		const actions = {
+			'admin': function(){
+				setShowAdd(true)
+			},
+			'user': function(){
+				setShowAdd(false)
+			}
+		}
+
+		const redirect = {
+			'error': '/#/'
+		}
+
+		const resp = await checkLogin()
+		const action = userControl(resp)
+		if(actions[action.action] !== undefined){
+			actions[action.action]()
+			setLoaded(true)
+			return true
+		}else{
+			window.location.href = redirect[action.action]
+			return false
+		}
+	}
+
+	React.useEffect(async ()=>{
+		setLoaded(await onLoad())
+	},[])
+
 	return(
 		<div className="flexColumn fullscreen bgcPrimary">
+		{!loaded && <LoadingPage/>}
+		{loaded && 
+		<>
 			<Header/>
+			{showAdd &&
 			<ContentBox>
 				<AddQuestion
 					subjectId={params.id}
 				/>
-			</ContentBox>
+			</ContentBox>}
 			<ListThemes
 				subjectId={params.id}
 			/>
+		</>}
 		</div>
 	)
 }
