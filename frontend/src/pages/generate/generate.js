0defc0c4e6184dbd83f1c96a5bc587fa2cbb8c4d
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-01 10:29:11 -0300

    Added background-color primary in pages/generate

--- frontend/src/pages/generate/generate.js
+++ frontend/src/pages/generate/generate.js
@@ -2,7 +2,7 @@ import { Header } from '../../components';
 
 export default function Generate({match:{params}}){
 	return(
-	<div id="generate" className="flexColumn fullscreen">
+	<div id="generate" className="flexColumn fullscreen bgcPrimary">
 		<Header/>
 	</div>
 	)
