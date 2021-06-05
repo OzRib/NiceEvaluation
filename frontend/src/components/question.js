efc741c4d80aab529ddf3ae6d90b6b2d7c6914ce
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-05 14:55:25 -0300

    Added list of themes in components/question

--- frontend/src/components/question.js
+++ frontend/src/components/question.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import { Modal, Badge } from 'react-bootstrap';
+import { ListThemeBadges } from './';
+
 export default function Question({questao, position}){
-	const { id, corpo, itens, resposta } = questao
+	const { id, corpo, itens, resposta, temas } = questao
+
+	const [show, setShow] = React.useState(false)
+	const [themes, setThemes] = React.useState(temas)
+
+	function deleteTheme(theme){
+		console.log('deleted', theme)
+	}
 
 	return(
-		<div className="border-transp border-rad20p flexColumn w100p po8 mt1p">
+	<>
+		<div 
+			className="border-transp border-rad20p flexColumn w100p po8 mt1p"
+			onClick={()=>{
+				setShow(true)
+			}}
+		>
 			<h5 className="boldText">Questão Nº {position}</h5>
 			<h6
 				className="text-muted"
@@ -23,5 +40,27 @@ export default function Question({questao, position}){
 				{resposta}
 			</p>
 		</div>
+		<Modal 
+			show={show}
+			onHide={()=>{
+				setShow(false)
+			}}
+		>
+			<Modal.Header closeButton>
+				<Modal.Title>
+					Questão {position}
+				</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				Temas:
+				<ListThemeBadges
+					themes={themes}
+					onDelete={theme =>{
+						deleteTheme(theme)
+					}}
+				/>	
+			</Modal.Body>
+		</Modal>
+	</>
 	)
 }
