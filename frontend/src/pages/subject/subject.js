f3a392cbabe1be3625d5c8fb77781faf86a74d01
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-16 21:47:16 -0300

    Added user control to header in pages/subject

--- frontend/src/pages/subject/subject.js
+++ frontend/src/pages/subject/subject.js
@@ -6,24 +6,29 @@ import { LoadingPage} from '../';
 
 export default function Subject({match:{params}}){
 	const [loaded, setLoaded] = React.useState(false)
+	const [admin, setAdmin] = React.useState(false)
 
 	async function onLoad(){
 		const actions = {
+			'admin': function(){
+				setAdmin(true)
+				return true
+			},
+			'user': function(){
+				setAdmin(false)
+				return true
+			},
 			'error': function(){
 				window.location.href = '/#/'
+				return false
 			}
 		}
 
 		const resp = await checkLogin()
 		const action = userControl(resp)
 
-		if(actions[action.action] !== undefined){
-			actions[action.action]()
-			return false
-		}else{
-			return true
-		}
-
+		if(actions[action.action] !== undefined)
+			return actions[action.action]()
 	}
 
 	React.useEffect(async ()=>{
