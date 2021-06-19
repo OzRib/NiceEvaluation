2ac88f95d01f5aa61dd441699c10dfee33dea6da
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-19 17:24:06 -0300

    Updating communication/sendtogenerate to support download updates

--- frontend/src/communication/sendtogenerate.js
+++ frontend/src/communication/sendtogenerate.js
@@ -1,18 +1,48 @@
-export default async function sendToGenerate(data, subjectId){
-	const themes = data.temas? JSON.stringify(data.temas): '' 
-	const general = data.geral || ''
+export default async function sendToGenerate(subjectId, data, downloadCallback){
+	return new Promise((resolve, reject)=>{
+		const req = new XMLHttpRequest()
+		req.open('POST', '/generate.php', true)
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+		req.responseType = 'json'
 
-	const dataToSend = `materia=${subjectId}&temas=${themes}&geral=${general}`
+		req.addEventListener('load', ()=>{
+			if(req.status !== 200){
+				reject('Erro no servidor: '+req.statusText)
+				return
+			}
 
-	const req = await fetch('/generate.php', {
-		method: 'POST',
-		headers:{
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		body: dataToSend
+			const resp = req.response
+			if(resp){
+				resolve(resp)
+			}else{
+				reject('Sem resposta')
+			}
+		}, false)
+
+		req.addEventListener('progress', (archive)=>{
+			if(downloadCallback){
+				const computable = archive.lengthComputable
+				const { loaded, total } = archive
+				const percent = (loaded*100)/total
+
+				if(computable && loaded>0){
+					downloadCallback['computable'] = true
+					downloadCallback['percent'] = percent
+				}else{
+					downloadCallback['computable'] = false
+				}
+			}
+		})
+
+		req.addEventListener('error', ()=>{
+			reject('Erro no ajax')
+		})
+
+		const themes = data.temas? JSON.stringify(data.temas): '[]'
+		const general = data.geral || '0'
+		
+		const dataToSend = `materia=${subjectId}&temas=${themes}&geral=${general}`
+
+		req.send(dataToSend)
 	})
-
-	const resp = await req.json()
-
-	return resp
 }
