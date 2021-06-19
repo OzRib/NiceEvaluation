42baaf7e06bf0b7b425671e4570c245eb190809e
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-19 17:30:02 -0300

    Moving generate response to component GenerateOptions

--- frontend/src/components/generateform.js
+++ frontend/src/components/generateform.js
@@ -7,56 +7,6 @@ export default function GenerateForm({subjectId}){
 	const [temas, setTemas] = React.useState([])
 	const [valorMaximo, setValorMaximo] = React.useState(0)
 
-	async function submit(event){
-		event.preventDefault()
-		
-		const form = event.target
-		const checkTemas = form['temas']
-		const haveThemes = checkTemas? checkTemas.checked: false
-
-		const themes = {}
-		
-		if(haveThemes){
-			for(let tema of temas){
-				const nome = tema.nome
-				themes[nome] = parseInt(form[nome].value)
-			}
-		}
-
-		const data = {
-			geral: parseInt(form['geral'].value),
-			temas: themes
-		}
-
-		const resp = await sendToGenerate(data, subjectId)
-
-		console.log(resp)
-
-		function base64ToUint8Array(str){
-			const byteStr = atob(str)
-
-			const byteNumbers = new Array(byteStr.length)
-			for(let x=0; x<byteStr.length; x++){
-				byteNumbers[x] = byteStr.charCodeAt(x)
-			}
-
-			const byteArray = new Uint8Array(byteNumbers)
-
-			return byteArray
-		}
-
-		const pdfUint8Array = base64ToUint8Array(resp.prova)
-
-		const pdf = new Blob([pdfUint8Array], {
-			type:'application/pdf'
-		})
-
-		const link = document.createElement('a')
-		link.href = window.URL.createObjectURL(pdf)
-		link.download = 'prova.pdf'
-		link.click()
-	}
-
 	async function loadData(){
 		const themes = await getThemes(subjectId)
 		const themesExtra = await getThemesExtra(subjectId)
@@ -95,9 +45,6 @@ export default function GenerateForm({subjectId}){
 		</h4>
 		<Form 
 			name="gerar"
-			onSubmit={event =>{
-				submit(event)
-			}}
 		>
 			<List
 				valorMaximo={valorMaximo}
