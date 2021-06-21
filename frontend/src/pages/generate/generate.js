873c9cbbc8f453a46279c6e6e234a05cceeb1f95
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-20 22:38:44 -0300

    Added user control in pages/generate

--- frontend/src/pages/generate/generate.js
+++ frontend/src/pages/generate/generate.js
@@ -1,16 +1,54 @@
+import React from 'react';
 import { Header, ContentBox, GenerateForm } from '../../components';
+import { checkLogin, userControl } from '../../communication';
+import { LoadingPage } from '../';
 
 export default function Generate({match:{params}}){
+	const [loaded, setLoaded] = React.useState(false)
+	const [admin, setAdmin] = React.useState(false)
 	const subjectId = params.id
+
+	async function onLoad(){
+		const actions = {
+			'admin': function(){
+				setAdmin(true)
+				return true
+			},
+			'user': function(){
+				setAdmin(false)
+				return true
+			},
+			'error': function(){
+				window.location.href = '/#/'
+				return false
+			}
+		}
+
+		const resp = await checkLogin()
+		const action = userControl(resp)
+		if(actions[action.action] !== undefined)
+			return actions[action.action]
+	}
+
+	React.useEffect(async ()=>{
+		setLoaded(await onLoad())
+	},[])
 
 	return(
 	<div className="flexColumn fullscreen bgcPrimary">
-		<Header/>
-		<ContentBox>
-			<GenerateForm
-				subjectId={subjectId}
+
+		{!loaded && <LoadingPage/>}
+		{loaded &&
+		<>
+			<Header
+				admin={admin}
 			/>
-		</ContentBox>
+			<ContentBox>
+				<GenerateForm
+					subjectId={subjectId}
+				/>
+			</ContentBox>
+		</>}
 	</div>
 	)
 }
