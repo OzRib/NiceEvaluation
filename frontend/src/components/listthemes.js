922fa62d4f20fd5bb608640e5ad9fae29ed9b48a
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-23 19:20:34 +0000

    Turned components/listthemes in a class && added a event listener to reload content

--- frontend/src/components/listthemes.js
+++ frontend/src/components/listthemes.js
@@ -3,28 +3,36 @@ import { Spinner } from 'react-bootstrap';
 import { Theme, ContentBox } from './';
 import { getThemes, getThemesExtra } from '../communication';
 
-export default function ListThemes({id, subjectId}){
-	const [loaded, setLoaded] = React.useState(false)
-	const [temas, setTemas] = React.useState([])
-	const [extra, setExtra] = React.useState({
-		semTema:null, 
-		todas:null
-	})
+export default class ListThemes extends React.Component{
+	constructor(props){
+		super(props)
 
-	async function loadThemes(){
+		this.state = {
+			loaded: false,
+			temas: [],
+			extra: {
+				semTema: null,
+				todas: null
+			}
+		}
+	}
+
+	async loadThemes(){
+		const subjectId = this.props.subjectId
 		const resp = await getThemes(subjectId)
 
 		console.log('Temas:', resp)
 
 		if(resp instanceof Array){
-			setTemas(resp)
+			this.setState({temas:resp})
 			return true
 		}else{
 			return false
 		}
 	}
 
-	async function loadExtra(){
+	async loadExtra(){
+		const subjectId = this.props.subjectId
 		const resp = await getThemesExtra(subjectId)
 
 		console.log('Extra', resp)
@@ -37,43 +45,56 @@ export default function ListThemes({id, subjectId}){
 		}
 	}
 
-	React.useEffect(async ()=>{
-		const loadedThemes = await loadThemes()
-		const loadedExtra = await loadExtra()
+	async loadData(){
+		const loadedThemes = await this.loadThemes()
+		const loadedExtra = await this.loadExtra()
 		const loaded = loadedThemes && loadedExtra
 
-		setLoaded(loaded)
-	},[])
+		this.setState({loaded:loaded})
+	}
 
-	return(
-	<ContentBox
-		className="flexColumn JCCenter AICenter"
-	>
-		{!loaded && <Spinner animation="border" variant="danger"/>}
-		{loaded && 
-		<>
-			<Theme
-				questions={extra.todas}
-				href={'/#/subject/'+subjectId+'/theme/all'}
-			>
-				Todas as questões
-			</Theme>
-			<Theme 
-				questions={extra.semTema}
-				href={'/#/subject/'+subjectId+'/theme/no-theme'}
-			>
-				Questões sem temas
-			</Theme>
-			{temas.map((obj, key)=>(
-				<Theme 
-					questions={obj.questoes} 
-					key={key}
-					href={'/#/subject/'+subjectId+'/theme/'+obj.id}
+	componentDidMount(){
+		document.addEventListener('reload', ()=>{
+			this.loadData()
+			this.forceUpdate()
+		})
+		this.loadData()
+	}
+
+	render(){
+		const subjectId = this.props.subjectId
+		const { loaded, temas, extra } = this.state
+		return(
+		<ContentBox
+			className="flexColumn JCCenter AICenter"
+		>
+			{!loaded && <Spinner animation="border" variant="danger"/>}
+			{loaded && 
+			<>
+				<Theme
+					questions={extra.todas}
+					href={'/#/subject/'+subjectId+'/theme/all'}
 				>
-					{obj.nome}
+					Todas as questões
 				</Theme>
-			))}
-		</>}
-	</ContentBox>
-	)
+				<Theme 
+					questions={extra.semTema}
+					href={'/#/subject/'+subjectId+'/theme/no-theme'}
+				>
+					
+					Questões sem temas
+				</Theme>
+				{temas.map((obj, key)=>(
+					<Theme 
+						questions={obj.questoes} 
+						key={key}
+						href={'/#/subject/'+subjectId+'/theme/'+obj.id}
+					>
+						{obj.nome}
+					</Theme>
+				))}
+			</>}
+		</ContentBox>
+		)
+	}
 }
