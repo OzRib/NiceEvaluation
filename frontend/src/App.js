50a6e3e64bbc435f9ba180d9d8e5d0a5c4250c0d
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-30 18:31:14 -0300

    Reorganized routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,20 +12,19 @@ import {
 	ShowQuestions,
 	Theme
 } from './pages';
+import { Login, AdminChoice } from './pages';
+import { LoggedRoutes } from './routes';
 
 function App() {
-  return (
-    <HashRouter>
-      <Route exact path='/' component={Login}/>
-      <Route exact path='/admin-choice' component={AdminChoice}/>
-      <Route exact path='/manage-users' component={ManageUsers}/>
-      <Route exact path='/teacher-page' component={TeacherPage}/>
-      <Route exact path='/subject/:id' component={Subject}/>
-      <Route exact path='/subject/:id/generate' component={Generate}/>
-      <Route exact path='/subject/:id/show-questions' component={ShowQuestions}/>
-      <Route exact path='/subject/:subjId/theme/:theme' component={Theme}/>
-    </HashRouter>
-  );
+	return (
+	<HashRouter>
+		<Route exact path='/' component={Login}/>
+		<Route exact path='/admin-choice' component={AdminChoice}/>
+		<Route path='/home'>
+			<LoggedRoutes/>
+		</Route>
+	</HashRouter>
+	);
 }
 
 export default App;
