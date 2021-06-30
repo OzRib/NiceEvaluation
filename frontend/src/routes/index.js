3a154cdcc8cdfa595d47d7a090b435487bd13b98
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-30 20:51:46 -0300

    Updated wrapper of routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,7 +1,9 @@
 import LoggedRoutes from './logged-routes';
 import AdminRoutes  from './admin-routes';
+import PrivilegedRoutes from './privileged-routes';
 
 export {
 	LoggedRoutes,
-	AdminRoutes
+	AdminRoutes,
+	PrivilegedRoutes
 }
