48ec8ccf7f9f08c5be73156671b5e4cc8d82b1b7
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-30 21:37:54 -0300

    Removed deprecated code in pages/show-questions

--- frontend/src/pages/show-questions/show-questions.js
+++ frontend/src/pages/show-questions/show-questions.js
@@ -3,43 +3,8 @@ import { Header, ContentBox, AddQuestion, ListThemes } from '../../components';
 import { LoadingPage } from '../';
 import { checkLogin, userControl } from '../../communication';
 
-export default function ShowQuestions({match:{params}}){
-	const [loaded, setLoaded] = React.useState(false)
-	const [admin, setAdmin] = React.useState(false)
-	const [showAdd, setShowAdd] = React.useState(false)
+export default function ShowQuestions({match:{params}, admin}){
 	const reload = new Event('reload')
-
-	async function onLoad(){
-		const actions = {
-			'admin': function(){
-				setShowAdd(true)
-				setAdmin(true)
-			},
-			'user': function(){
-				setShowAdd(false)
-				setAdmin(false)
-			}
-		}
-
-		const redirect = {
-			'error': '/#/'
-		}
-
-		const resp = await checkLogin()
-		const action = userControl(resp)
-		if(actions[action.action] !== undefined){
-			actions[action.action]()
-			setLoaded(true)
-			return true
-		}else{
-			window.location.href = redirect[action.action]
-			return false
-		}
-	}
-
-	React.useEffect(async ()=>{
-		setLoaded(await onLoad())
-	},[])
 
 	return(
 		<div className="flexColumn fullscreen bgcPrimary">
