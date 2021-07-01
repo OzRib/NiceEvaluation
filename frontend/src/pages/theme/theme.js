40af66f6af6806fc96578b6c36eced1889593854
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-06-30 21:40:42 -0300

    Removed deprecated code in pages/theme

--- frontend/src/pages/theme/theme.js
+++ frontend/src/pages/theme/theme.js
@@ -4,36 +4,7 @@ import { checkLogin, userControl } from '../../communication';
 import { LoadingPage } from '../';
 
 export default function Theme({match:{params}}){
-	const [loaded, setLoaded] = React.useState(false)
-	const [admin, setAdmin] = React.useState(false)
 	const { subjId, theme } = params
-
-	async function onLoad(){
-		const actions = {
-			'admin': function(){
-				setAdmin(true)
-				return true
-			},
-			'user': function(){
-				setAdmin(false)
-				return true
-			},
-			'error': function(){
-				window.location.href = '/#/'
-				return false
-			}
-		}
-
-		const resp = await checkLogin()
-		const action = userControl(resp)
-
-		if(actions[action.action] !== undefined)
-			return actions[action.action]()
-	}
-
-	React.useEffect(async ()=>{
-		setLoaded(await onLoad())
-	},[])
 
 	return(
 		<div className="flexColumn fullscreen bgcPrimary">
